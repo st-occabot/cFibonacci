601c47ac7b0c5ab17c78ed019814a82640113546
occabot <sampletext.occabot@gmail.com>
2015-02-28 16:14:47 +0000

    Added initial code and the ability to input the amount of numbers of the sequence to be printed out.

---
+++ fibonacci.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Class to output the fibonacci sequence via the C programming language.
+// Made this for the lulz. - occabot
+
+int main(int argc, char *argv[]) {
+	int first, second, currentValue, sequenceSize;
+	
+	printf("Please enter how many values of the fibonacci sequence you would like: ");
+	scanf("%d", &sequenceSize);
+	printf("You entered: %d.\n", sequenceSize);
+
+	return 0;
+}
