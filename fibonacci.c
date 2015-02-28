00182ee54ef2a6105f88ce248013deaaa07f6e5b
occabot <sampletext.occabot@gmail.com>
2015-02-28 17:09:30 +0000

    Patched the integer overflow possibility; commented to explain the code

--- fibonacci.c
+++ fibonacci.c
@@ -11,21 +11,24 @@ int main(int argc, char *argv[]) {
 	//Grabs the amount of values of the sequence to print out
 	printf("Please enter how many values of the fibonacci sequence you would like: ");
 	scanf("%d", &sequenceSize);
-	//printf("You entered: %d.\n", sequenceSize);
+	
+	//Prevents integer overflows or sequences less than at least 1.
+	while ((sequenceSize < 1) || (sequenceSize > 46)) {
+		printf("Incorrect value! Please enter a number between 1 and 46 inclusive: ");
+		scanf("%d", &sequenceSize);
+	}
 
 	//Print out values until the size inputted has been reached
 	for (int count = 0; count < sequenceSize; count++) {
-		//Print out 1 for the first 2 values of the sequence
-		if (count == 0) {
+		if (count == 0) { //Prints out 1 for the first value of the sequence
 			printf("1 "); 
-		} else if (count == 1) {
+		} else if (count == 1) { //Prints out 1 for the second value
 			printf("1 ");
-		} else {	
-			currentValue = firstValue + secondValue;
-			printf("%d ", currentValue);
-			//secondValue = currentValue - firstValue;
-			firstValue = secondValue;
-			secondValue = currentValue;
+		} else { // Prints out the next value of the sequence
+			currentValue = firstValue + secondValue; // The next value is the previous two added together
+			printf("%d ", currentValue); // Prints out the combined value
+			firstValue = secondValue; // firstValue becomes the value before currentValue
+			secondValue = currentValue; // secondValue becomes the next value in the sequence
 		}
 	}
 
